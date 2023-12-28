96a9fd0a721116036e78c4cd1790304bdcbb7e80
Hart <hart.hpwebdev@gmail.com>
2023-12-28 11:43:26 +0800

    opening message and base of operations

--- main.c
+++ main.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float add(num1, num2)
+float add(float num1, float num2)
 {
-
+    return num1 + num2;
 }
 
-float subtract(num1, num2)
+float subtract(float num1, float num2)
 {
-    
+    return num1 - num2;
 }
 
-float multiply(num1, num2)
+float multiply(float num1, float num2)
 {
-    
+    return num1 * num2;
 }
 
-float divide(num1, num2)
+float divide(float num1, float num2)
 {
-    
+    return num1 / num2;
 }
 
 int main()
 {
     float num1;
     float num2;
-    
+    int operation;
+    float answer;
+
+    system("clear");
+    printf("\n*********** CALCULATOR ***********\n");
+    printf("\nChoose your operation by picking the number:\n\n");
+    printf("1. Addition     3. Multiplication\n");
+    printf("2. Subtraction     4. Division\n");
+
     return 0;
 }
